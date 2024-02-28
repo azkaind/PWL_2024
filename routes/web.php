fe73065d84401a317aef67854bc624571c1d35ce
azkaind <azka06.07.2004@gmail.com>
2024-02-28 22:51:59 +0700

    view

--- routes/web.php
+++ routes/web.php
@@ -71,3 +71,9 @@ Route::resource('photos', PhotoController::class)->only([ 'index', 'show'
 
 Route::resource('photos', PhotoController::class)->except([ 'create', 'store', 'update', 'destroy'
 ]);
+
+Route::get('/greeting', function () {
+    return view('blog.hello', ['name' => 'Azka']);
+});
+
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
